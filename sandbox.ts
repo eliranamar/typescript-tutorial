b83085efa34e0ee9eecd5d6993b2bdce784caff9
Eliran Amar <eliran.amar@mayple.com>
2020-11-20 22:13:49 +0200

    type basics

---
+++ sandbox.ts
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach((input) => {
+  console.log(input);
+})
+
+const circ = (diameter: number) => {
+
+  return diameter * Math.PI;
+
+}
+
+console.log(circ(3));
